62f60d1666849399c0d6b1460ad79f486a34693d
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2024-05-05 16:53:57 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from './config/typeorm';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [typeOrmConfig],
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) =>
+      configService.get('typeorm'),
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
